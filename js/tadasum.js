3b770097e2bbdb1122422b8834982cbb95492c22
Ahmad Arif Nuhuda <151599766+ahmadarifnhd@users.noreply.github.com>
2024-10-16 10:25:36 +0700

    Update tadasum.js

--- js/tadasum.js
+++ js/tadasum.js
@@ -3,9 +3,9 @@ let food2s = document.getElementById('food2');
 let foods = document.getElementById('food');
 
 food1s.addEventListener('click',()=>{
-    foods.style.backgroundImage="url('/TADASUM/image/Menu1.png')";
+    foods.style.backgroundImage="url('/image/Menu1.png')";
 })
 
   food2s.addEventListener('click', () => {
-    foods.style.backgroundImage = "url('/TADASUM/image/Menu2.png')";
+    foods.style.backgroundImage = "url('/image/Menu2.png')";
 })
