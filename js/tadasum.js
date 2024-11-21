62afdeb493aec029214806cc487831d1f1c1a11a
Ahmad Arif Nuhuda <151599766+ahmadarifnhd@users.noreply.github.com>
2024-11-21 14:06:56 +0700

    update tadasum.js

--- js/tadasum.js
+++ js/tadasum.js
@@ -1,7 +1,5 @@
 let food1s = document.getElementById('food1');
 let food2s = document.getElementById('food2');
-let food3s = document.getElementById('food3');
-let food4s = document.getElementById('food4');
 let foods = document.getElementById('food');
 
 food1s.addEventListener('click',()=>{
@@ -10,11 +8,4 @@ food1s.addEventListener('click',()=>{
 
   food2s.addEventListener('click', () => {
     foods.style.backgroundImage = "url('/image/Menu2.png')";
-})
-food1s.addEventListener('click',()=>{
-    foods.style.backgroundImage="url('/image/Menu3.png')";
-})
-
-  food2s.addEventListener('click', () => {
-    foods.style.backgroundImage = "url('/image/Menu4.png')";
 })
