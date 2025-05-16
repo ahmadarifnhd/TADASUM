dc6936d8300628b3be212557abd2f5f18c87f99d
unknown <mbkm24gn0013@mhs.usk.ac.id>
2025-05-16 09:19:26 +0700

    new commit

--- js/tadasum.js
+++ js/tadasum.js
@@ -2,10 +2,12 @@ let food1s = document.getElementById('food1');
 let food2s = document.getElementById('food2');
 let foods = document.getElementById('food');
 
-food1s.addEventListener('click',()=>{
-    foods.style.backgroundImage="url('/image/Menu1.png')";
-})
+if (food1s && food2s && foods) {
+    food1s.addEventListener('click', () => {
+        foods.style.backgroundImage = "url('image/Menu1.png')";
+    });
 
-  food2s.addEventListener('click', () => {
-    foods.style.backgroundImage = "url('/image/Menu2.png')";
-})
+    food2s.addEventListener('click', () => {
+        foods.style.backgroundImage = "url('image/Menu2.png')";
+    });
+}
